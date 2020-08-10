868431853f16c555fbdce97bce406293f4dcf6ff
iannakim <dear.hyunji@gmail.com>
2020-08-10 15:40:11 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,61 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+
+# * `Gallery#paintings`
+# * Returns an `array` of all paintings in my gallery
+
+def paintings
+   Painting.all.select{|painting| painting.gallery == self}
+  end
+
+
+# * `Gallery#artists`
+# * Returns an `array` of all artists that have a painting in my gallery
+
+  def artists
+    artist_array = paintings.map {|painting| painting.artist}
+    artist_array.uniq
+  end
+
+
+# * `Gallery#artist_names`
+# * Returns an `array` of the names of all artists that have a painting in my gallery  
+  def artist_names
+      artists.map {|artist_inst| artist_inst.name}
+  end
+
+
+
+# * `Gallery#most_expensive_painting`
+# * Returns an `instance` of the most expensive painting in a gallery
+
+
+  def most_expensive_painting
+    price_array = Painting.all.map {|painting| painting.price}
+      max_price = price_array.max
+
+      Painting.all.find {|painting| painting.price == max_price}
+  end
+
+
+
 end
+
+
+
+
+
+
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,36 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery_inst, artist_inst)
     @title = title
     @price = price
+    @artist = artist_inst
+    @gallery = gallery_inst
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+
+
+# * `Painting.total_price`
+# * Returns an `integer` that is the total price of all paintings
+  def self.total_price
+   price_array = self.class.all.map {|painting| painting.price}
+    sum = price_array.sum
+    sum
+  end
+
+
+  # def self.total_price
+  #   Painting.all.sum(&:price)
+  # end 
+
 end
+
+
